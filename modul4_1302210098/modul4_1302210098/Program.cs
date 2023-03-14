3d80c709c4f2161c842c3ed414d5c39494e4cda2
alifrasyid <alifrasyid.271003@gmail.com>
2023-03-14 09:23:43 +0700

    menambahkan class KodeBuah

--- modul4_1302210098/modul4_1302210098/Program.cs
+++ modul4_1302210098/modul4_1302210098/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class KodeBuah
+{
+    public enum NamaBuah { Apel, Aprikot, Alpukat, Pisang, paprika, Blackberry, Ceri, Kelapa, Jagung, Kurma, Durian, Anggur, Melon, Semangka}
+    public static string getKodeBuah(NamaBuah buah)
+    {
+        string[] kodebuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+        return kodebuah[(int)buah];
+    }
+}
