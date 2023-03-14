c3da1cb43ef7f3913270f8cb695009e25f849c1a
alifrasyid <alifrasyid.271003@gmail.com>
2023-03-14 10:17:39 +0700

    menambahkan class PosisiKarakterGame

--- modul4_1302210098/modul4_1302210098/Program.cs
+++ modul4_1302210098/modul4_1302210098/Program.cs
@@ -9,3 +9,81 @@ public class KodeBuah
         return kodebuah[(int)buah];
     }
 }
+
+public class PosisiKarakterGame
+{
+    enum state { Berdiri, Jongkok, Tengkurap, Terbang}
+    state posisi = state.Berdiri;
+    string perintah = "w";
+
+    public void ChangState()
+    {
+        Console.WriteLine(posisi);
+        while (perintah == "w" || perintah == "s" || perintah == "x")
+        {
+            Console.Write("Masukkan Perintah: ");
+            perintah = Console.ReadLine();
+            if (posisi == state.Berdiri)
+            {
+                if(perintah == "w")
+                {
+                    posisi = state.Terbang;
+                    Console.WriteLine(posisi);
+                    Console.WriteLine("posisi take off");
+                }else if(perintah == "s")
+                {
+                    posisi = state.Jongkok;
+                    Console.WriteLine(posisi);
+                }
+            }else if (posisi == state.Terbang)
+            {
+                if (perintah == "x")
+                {
+                    posisi = state.Jongkok;
+                    Console.WriteLine(posisi);
+                    Console.WriteLine("posisi landing");
+                }
+                else if (perintah == "s")
+                {
+                    posisi = state.Berdiri;
+                    Console.WriteLine(posisi);
+                }
+            }
+            else if (posisi == state.Jongkok)
+            {
+                if (perintah == "w")
+                {
+                    posisi = state.Berdiri;
+                    Console.WriteLine(posisi);
+                }
+                else if (perintah == "s")
+                {
+                    posisi = state.Tengkurap;
+                    Console.WriteLine(posisi);
+                }
+            }
+            else if (posisi == state.Tengkurap)
+            {
+                if (perintah == "w")
+                {
+                    posisi = state.Jongkok;
+                    Console.WriteLine(posisi);
+                }
+            }
+        }
+    }
+}
+class MainClass
+{
+    static void Main()
+    {
+        KodeBuah.NamaBuah buah = KodeBuah.NamaBuah.Kurma;
+        string kodebuah = KodeBuah.getKodeBuah(KodeBuah.NamaBuah.Kurma);
+        Console.WriteLine("Kode buah untuk buah " + buah + " adalah " + kodebuah);
+
+        PosisiKarakterGame pkg = new PosisiKarakterGame();
+        Console.WriteLine("Ketetntuan Perintah: w,s,x");
+        pkg.ChangState();
+    }
+    
+}
